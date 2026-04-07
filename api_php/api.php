5ea815fe3dd28dec0e64137f5ffce41129454359
Viniciusdossantosandrade <162480502+Viniciusdossantosandrade@users.noreply.github.com>
2026-04-07 16:12:51 -0300

    Add files via upload

--- api_php/api.php
+++ api_php/api.php
@@ -1,10 +1,27 @@
 <?php
    header("Content-Type: application/json");
 
-   $usuarios = [
-    ["id" => 1, "nome" => "Maria souza", "email"  => "maria@email.com"],
-    ["id" => 2, "nome" => "Joao henrique", "email"  => "joao@email.com"],
-   ]
+   $metodo = $_SERVER['REQUEST_METHOD'];
 
-   echo json_encode($usuarios);
+   //echo "Metodo da requisição: ".$metodo;
+
+   switch ($metodo){
+    case 'GET' :
+        echo "AQUI AÇOES DO METODO GET";
+        break;
+    case 'POST' :
+        echo "AQUI AÇOES DO METODO GET";
+        break;    
+    default :
+        echo "METODO NAO ENCONTRADO!";
+        break;
+    
+   }
+
+  // $usuarios = [
+   // ["id" => 1, "nome" => "Maria souza", "email"  => "maria@email.com"],
+    //["id" => 2, "nome" => "Joao henrique", "email"  => "joao@email.com"]
+   //];
+
+   //echo json_encode($usuarios);
 ?>
